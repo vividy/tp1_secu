60c1a848e71623df60799838c35084f9a8111e23
Alex <alexandre.hermitant@epitech.eu>
2017-02-20 17:03:38 -0500

    01

--- Q2_2/main.cpp
+++ Q2_2/main.cpp
@@ -13,11 +13,12 @@ string	recurs(string str, string stealPass, string hash, int pos)
 
 	if (md5(hash) == stealPass)
 	{
-		return (hash);
+	  cout << "Password found:" << hash << endl;		
+	  return (hash);
 	}
 	if (pos > 7)
 		return ("");
-	//      	cout << "pos: " << pos << "\t hash:" << hash << "\t stealPass:" << stealPass << endl;
+	//	cout << "pos: " << pos << "\t hash:" << hash << "\t stealPass:" << stealPass << endl;
 	for (int i = 0; i < str.length(); ++i)
 	{
 		tmp.clear();
@@ -40,22 +41,22 @@ int		main(int ac, char **av)
 	string		stealPass;
 	string		str("abcdefghijklmnopqrstuvwxyz0123456789!@#$%%&*");
 	string		hash("");
-	thread          calc[46];
+	thread          calc[8];
 
 	if (ac > 1)
 		stealPass = av[1];
 	else
 	  return (-1);
-	for (int y = 0; y < 8; ++y)
+	for (int y = 0; y < 7; ++y)
 	{
 	  calc[y] = thread(recurs, ref(str), ref(stealPass), ref(hash), ref(y));
 	  // for (int i = 0; i < str.length(); ++i)
 	  //   {
 	  //   }
 	}
-
-	// if ((hash = recurs(str, stealPass, hash, 0)) != "")
-	// 	cout << "Password found:" << hash << endl;
+	for (int y = 0; y < 7; y++)
+	  calc[y].join();
+	// if (hash != "")
 	// else
 	// 	cout << "Password not found..." << endl;
 	return (0);
