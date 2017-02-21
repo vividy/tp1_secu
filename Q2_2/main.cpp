931428190fcbefe83a3a3b9d8de28b1cc9e3d7be
vividy <vivien.pradelles@gmail.com>
2017-02-21 16:02:40 -0500

    optimisation de fou mon ga!

--- Q2_2/main.cpp
+++ Q2_2/main.cpp
@@ -12,15 +12,15 @@ using namespace std;
 int	isFined  = 5;
 mutex	isFined_mutex;
 
-string	recurs(string &str, string *stealPass, string &hash, int pos, int &num, int pow)
+void	recurs(string &str, string *stealPass, string &hash, int pos, int &num, int pow)
 {
-	string	end;
 	string	tmp;
-
+	string	myMd5;
 //	cout << "num:" << num << "\tpos:" << pos << "\t hash:" << hash << endl;
-	for (int i = 0; i < stealPass->size(); ++i)
+	myMd5 = md5(hash);
+	for (int i = 0; i < 5; ++i)
 	{
-		if (md5(hash) == stealPass[i])
+		if (myMd5 == stealPass[i])
 		{
 			isFined_mutex.lock();
 			isFined -= 1;
@@ -33,24 +33,25 @@ string	recurs(string &str, string *stealPass, string &hash, int pos, int &num, i
 	}
 
 	if (pos > 6)
-		return ("");
-	for (unsigned int i = pow; i < str.length(); i++)
+		return ;
+	else if (pos < 4)
+		cout << "thread no: " << num << "\thash actual: " << hash << endl;
+	for (unsigned int i = pow; i < 43; i++)
 	{
-		isFined_mutex.lock();
-		if (isFined == 0)
+		if (isFined_mutex.try_lock() == true)
 		{
+			if (isFined == 0)
+			{
+				isFined_mutex.unlock();
+				return ;
+			}
 			isFined_mutex.unlock();
-			return ("");
 		}
-		isFined_mutex.unlock();
 		tmp.clear();
 		tmp = hash;
 		tmp += str[i];
-		end = recurs(str, stealPass, tmp, pos + 1, num, pow);
-		if (end != "")
-			return (end);
+		recurs(str, stealPass, tmp, pos + 1, num, pow);
 	}
-	return ("");
 }
 
 void	threadHandler(string str, string *stealPass, string hash, int num, int pow)
@@ -64,25 +65,22 @@ int		main(int ac, char **av)
 	string		str("abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*");
 	string		hash("");
 	thread		*calc = new thread[str.size()];
-	thread		*calcx = new thread[str.size()];
 	
 	stealPass[0] = "3692af23f6e25ff3e4f6fc07d1952267";
 	stealPass[1] = "b8a1c63b24574a4b5f041cc6940035e9";
 	stealPass[2] = "eeb7a3effbc7883db281a859bfa8b8f9";
 	stealPass[3] = "15a196696cc990f8796871a1001eb21f";
-	//stealPass[4] = "c2b7443aeba6de69bcba5af76c5bfd5a";
+	// stealPass[4] = "a98a747db71f95aa57506d2aa7f07e50";
 	stealPass[4] = "ee164042f1ac00236cc00e3a46b37d15";
 
 	for (unsigned int i = 0; i < str.size(); ++i)
 	{
 		hash = str[i];
 		calc[i] = thread(&threadHandler, str, stealPass, hash, i, 0);
-		//calcx[i] = thread(&threadHandler, str, stealPass, hash, i, 1);
 	}
 	for (unsigned int i = 0; i < str.size(); ++i)
 	{
 		calc[i].join();
-		//calcx[i].join();
 	}
 
 	return (0);
